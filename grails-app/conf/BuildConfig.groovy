6c0a242e9138f2067d4074c9c052d504536b8cc9
Jean-Pierre Froud <jpfroud@gmail.com>
2014-11-03 14:01:59 +0100

    USPREAD-57 - Serveur - Heroku : brancher sur bdd postgres #time 30m #close

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -67,6 +67,8 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+        // TODO : supprimer après migration vers mongodb
+        runtime 'postgresql:postgresql:8.4-702.jdbc3'
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
